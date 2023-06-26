9ec2e3fa77bdd9f4dc3a8177f81ac10b690358dc
emareaun <emareaun12345@gmail.com>
2023-06-26 19:55:37 -0400

    js updated

--- java.js
+++ java.js
@@ -78,4 +78,15 @@ function renderCities() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+      cityTitle = $("<h3>").text(response.name + " "+ FormatDay());
+      $("#today-weather").append(cityTitle);
+      var TempetureToNum = parseInt((response.main.temp)* 9/5 - 459);
+      var cityTemperature = $("<p>").text("Tempeture: "+ TempetureToNum + " °F");
+      $("#today-weather").append(cityTemperature);
+      var cityHumidity = $("<p>").text("Humidity: "+ response.main.humidity + " %");
+      $("#today-weather").append(cityHumidity);
+      var cityWindSpeed = $("<p>").text("Wind Speed: "+ response.wind.speed + " MPH");
+      $("#today-weather").append(cityWindSpeed);
+      var CoordLon = response.coord.lon;
+      var CoordLat = response.coord.lat;
       
