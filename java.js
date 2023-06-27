c586d1afc6cd0e478676fc21a44939cd18a6e1eb
emareaun <emareaun12345@gmail.com>
2023-06-26 20:31:16 -0400

    js updated

--- java.js
+++ java.js
@@ -103,3 +103,76 @@ function renderCities() {
         if(responseuv.value > 0 && responseuv.value <=2){
             cityUV.attr("class","green")
         }
+        else if (responseuv.value > 2 && responseuv.value <= 5){
+            cityUV.attr("class","yellow")
+        }
+        else if (responseuv.value >5 && responseuv.value <= 7){
+            cityUV.attr("class","orange")
+        }
+        else if (responseuv.value >7 && responseuv.value <= 10){
+            cityUV.attr("class","red")
+        }
+        else{
+            cityUV.attr("class","purple")
+        }
+    });
+    var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + key;
+    $.ajax({
+    url: queryURL3,
+    method: "GET"
+}).then(function(response5day) { 
+    $("#boxes").empty();
+    console.log(response5day);
+    for(var i=0, j=0; j<=5; i=i+6){
+        var read_date = response5day.list[i].dt;
+        if(response5day.list[i].dt != response5day.list[i+1].dt){
+            var FivedayDiv = $("<div>");
+            FivedayDiv.attr("class","col-3 m-2 bg-primary")
+            var d = new Date(0); 
+            d.setUTCSeconds(read_date);
+            var date = d;
+            console.log(date);
+            var month = date.getMonth()+1;
+            var day = date.getDate();
+            var dayOutput = date.getFullYear() + '/' +
+            (month<10 ? '0' : '') + month + '/' +
+            (day<10 ? '0' : '') + day;
+            var Fivedayh4 = $("<h6>").text(dayOutput);
+            //Set src to the imags
+            var imgtag = $("<img>");
+            var skyconditions = response5day.list[i].weather[0].main;
+            if(skyconditions==="Clouds"){
+                imgtag.attr("src", "https://img.icons8.com/color/48/000000/cloud.png")
+            } else if(skyconditions==="Clear"){
+                imgtag.attr("src", "https://img.icons8.com/color/48/000000/summer.png")
+            }else if(skyconditions==="Rain"){
+                imgtag.attr("src", "https://img.icons8.com/color/48/000000/rain.png")
+            }
+
+            var pTemperatureK = response5day.list[i].main.temp;
+            console.log(skyconditions);
+            var TempetureToNum = parseInt((pTemperatureK)* 9/5 - 459);
+            var pTemperature = $("<p>").text("Tempeture: "+ TempetureToNum + " °F");
+            var pHumidity = $("<p>").text("Humidity: "+ response5day.list[i].main.humidity + " %");
+            FivedayDiv.append(Fivedayh4);
+            FivedayDiv.append(imgtag);
+            FivedayDiv.append(pTemperature);
+            FivedayDiv.append(pHumidity);
+            $("#boxes").append(FivedayDiv);
+            console.log(response5day);
+            j++;
+        }
+    
+}
+
+});
+
+
+});
+
+}
+
+$(document).on("click", "#listC", function() {
+var thisCity = $(this).attr("data-city");
+getResponseWeather(thisCity);
+});
